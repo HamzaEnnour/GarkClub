84f233d4f6753e42f9edc56a65f08d3fe07c4b65
HamzaEnnour <49129326+HamzaEnnour@users.noreply.github.com>
2022-01-02 15:32:27 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,33 +2,10 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+
+//private
 export const environment = {
 	production: true,
 	SCARF_ANALYTICS: false,
-	adminRoot: '/football/app',
-	apiUrl: 'https://gark-academy-api.herokuapp.com/api',
-	backend: 'https://gark-academy-api.herokuapp.com/',
-	defaultMenuType: 'menu-default',
-	subHiddenBreakpoint: 1440,
-	menuHiddenBreakpoint: 768,
-	themeColorStorageKey: 'gark-themecolor',
-	defaultColor: 'light.greenlime',
-	darkTheme: 'dark.greenlime',
-	isDarkSwitchActive: true,
-	defaultDirection: 'ltr',
-	themeRadiusStorageKey: 'gark-themeradius',
-	isAuthGuardActive: true,
-	remember: 'SYS_SEC',
-	rememberSecret: 'eedaipiichah1Ahv4ug0oiriel6oop6rae2vodopoohaiphoghu9quaengaedi6g',
-	firebase: {
-		apiKey: 'AIzaSyAW5XI1xtyDSCwXqgJxKY_bzLTJEHISoxo',
-		authDomain: 'gark-b5d29.firebaseapp.com',
-		projectId: 'gark-b5d29',
-		storageBucket: 'gark-b5d29.appspot.com',
-		messagingSenderId: '482874847853',
-		appId: '1:482874847853:web:047579139ad926d0b78564',
-		measurementId: 'G-VQXZB5PNJ6',
-		token:
-			'AAAAMBE-Oqo:APA91bHIzAzm_epVJWrL0C05jKiI6DZ4kdm-fxmTJUP4oSBoWJeEpyOFp2XyuMLKeMXjuigid76WwYKQTlrzhWZZbu8NLUPXa-g20eI24UwuOE_JTAmYo4qJxHMIlf_LidhK7QrBNQws'
-	}
 };
